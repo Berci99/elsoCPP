5c52f32a922fd994273cbb85f5c4f0c5a3da6993
Berci99 <39827133+Berci99@users.noreply.github.com>
2019-02-18 11:02:27 +0100

    Create dolgozat0210.cpp

---
+++ dolgozat0210.cpp
@@ -0,0 +1,35 @@
+// created by Berci. class: 12/D
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Auto {
+public:
+	string LPT981;
+	string JAB701;
+	string ITT604;
+ 
+	Auto(){}
+	Auto(string _LPT981,  string _JAB701,   string _ITT604) {
+		LPT981 = _LPT981;
+		JAB701 = _JAB701;
+		ITT604 = _ITT604;
+
+
+	}
+
+
+};
+// kell a vektor
+
+// meg kell nyitni a fajlt
+// majd kiolvasni
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
